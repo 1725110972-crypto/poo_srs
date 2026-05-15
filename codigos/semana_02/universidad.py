18dfb16b73dee59ea16249f14f84b3befba64b0c
Stephanie Santander <1725110972@utectulancingo.edu.mx>
2026-05-15 02:50:25 +0000

    CREATED codigo codigos.py

---
+++ codigos/semana_02/universidad.py
@@ -0,0 +1,28 @@
+class Universidad:
+    def__int__(self, logo, oferta_educativa, localidad, sistema_informatica, modalidad, servicios, ubicacion, talleres, cantidad_salones, rector)
+            self.logo=logo
+            self.oferta_educativa=oferta_educativa
+            self.localidad=localidad
+            self.sistema_informatica
+            self.modalidad=modalidad
+            self.servicios=servicios
+            self.ubicacion=ubicacion
+            self.talleres=talleres
+            self.cantidad_salones
+            self.rector=rector
+
+            print(f"Logotipo de la Universidad:{self.logo}")
+            print(f"Oferta Educativa:{self.oferta_educativa}")
+            print(f"Localidad de la Universidad:{self.localidad}")
+            print(f"Sistema Informatico:{self.sistema_informatico}")
+            print(f"Modalidad de la Univerdidad:{self.modalidad}")
+            print(f"Servicios de la Universidad:{self.servicios}")
+            print(f"Ubicacion de la Universidad:{self.ubicacion}")
+            print(f"Talleres de la Universidad:{self.talleres}")
+            print(f"Cantidad de Salones de la Universidad:{self.cantidad_salones}")
+            print(f"Rector de la Universidad:{self.rector}")
+
+    unideh=Universidad("logo.jpg","ing.Software,Turismo","San Miguel","CADU","Virtual","Biblioteca Digital","Santa Catarina",None,None"Octavio Casillo")    
+
+    
+    
